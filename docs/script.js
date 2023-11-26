6aec5df1c5dfa2e1b4ac2c572fa195397dbd458e
gmpiler <gondos.waseda@gmail.com>
2023-11-27 01:21:07 +0900

    [Fix] chang the character's image

--- docs/script.js
+++ docs/script.js
@@ -12,8 +12,8 @@ window.onload = function () {
 	game.preload([clickSndUrl]); 				//データを読み込んでおく
 
 	//ぞう山くん画像
-	const zoyamaImgUrl = "zoyama.png";						//game.htmlからの相対パス
-	game.preload([zoyamaImgUrl]);					//データを読み込んでおく
+	const kirimochiImgUrl = "kirimochi.png";						//game.htmlからの相対パス
+	game.preload([kirimochiImgUrl]);					//データを読み込んでおく
 
 	//リトライボタン
 	const retryImgUrl = "retry.png";						//game.htmlからの相対パス
@@ -54,18 +54,28 @@ window.onload = function () {
 
 		scoreText.text = "現在：" + point;					//テキストに文字表示 Pointは変数なので、ここの数字が増える
 
-		//ぞう山ボタン
-		const zoyamaImg = new Sprite(166, 168);				//画像サイズをここに書く。使う予定の画像サイズはプロパティで見ておくこと
-		zoyamaImg.moveTo(118, 100);						//ぞう山ボタンの位置
-		zoyamaImg.image = game.assets[zoyamaImgUrl];			//読み込む画像の相対パスを指定。　事前にgame.preloadしてないと呼び出せない
-		mainScene.addChild(zoyamaImg);					//mainSceneにこのぞう山画像を貼り付ける  
+		//ポイント表示テキスト2
+		const sampleText = new Label();
+		sampleText.font = "20px Meiryo"
+		sampleText.color = 'rgba(100, 100, 100, 5)'
+		sampleText.width = 200;
+		sampleText.moveTo(point, point);
+		mainScene.addChild(sampleText);
 
-		zoyamaImg.ontouchend = function () {				//ぞう山ボタンをタッチした（タッチして離した）時にこの中の内容を実行する
+		sampleText.text = "sampleText" + point;
+
+		//ぞう山ボタン
+		const kirimochiImg = new Sprite(150, 138);				//画像サイズをここに書く。使う予定の画像サイズはプロパティで見ておくこと
+		kirimochiImg.moveTo(118, 100);						//ぞう山ボタンの位置
+		kirimochiImg.image = game.assets[kirimochiImgUrl];			//読み込む画像の相対パスを指定。　事前にgame.preloadしてないと呼び出せない
+		mainScene.addChild(kirimochiImg);					//mainSceneにこのぞう山画像を貼り付ける
+
+		kirimochiImg.ontouchend = function () {				//ぞう山ボタンをタッチした（タッチして離した）時にこの中の内容を実行する
 			point++;									//Pointを1増やす
 			game.assets[clickSndUrl].clone().play();		//クリックの音を鳴らす。
 
 			//クリックしたのでぞう山画像のｘ位置を戻す
-			this.x = -200;							//this.xって何？と思った方、Zoyamaの関数内でぞう山の座標を動かすときにはthisを使います。
+			this.x = -200;							//this.xって何？と思った方、kirimochiの関数内でぞう山の座標を動かすときにはthisを使います。
 
 			//ポイントによって状態Stateを変更する
 			if (point < 3) {
@@ -88,38 +98,38 @@ window.onload = function () {
 		//メインループ　ここに主要な処理をまとめて書こう
 		game.onenterframe = function () {
 			if (state == 0) { 							//state=0のとき、初期セット状態(Pointの状態を０にして)
-				zoyamaImg.x = -200;						//ぞう山のｘ座標を指定
-				zoyamaImg.y = 100;						//ぞう山のy座標を指定
+				kirimochiImg.x = -200;						//ぞう山のｘ座標を指定
+				kirimochiImg.y = 100;						//ぞう山のy座標を指定
 				point = 0;  							//point初期化
 				state = 1;							//ゲームスタート状態に移行
 			}
 			if (state == 1) {							//ゲームスタート　状態１
-				zoyamaImg.x += 5;
+				kirimochiImg.x += 5;
 			}
 			if (state == 2) {							//状態２（point３以上なら）
-				zoyamaImg.x += 15;
+				kirimochiImg.x += 15;
 			}
 			if (state == 3) {							//状態３（point６以上から）
-				zoyamaImg.x += 10;
-				zoyamaImg.y = 200 + Math.sin(zoyamaImg.x / 70) * 100; // ｙ座標を振幅100pxのサイン波で移動(Sinは便利なので慣れとくといいよ！)
+				kirimochiImg.x += 10;
+				kirimochiImg.y = 200 + Math.sin(kirimochiImg.x / 70) * 100; // ｙ座標を振幅100pxのサイン波で移動(Sinは便利なので慣れとくといいよ！)
 			}
 			if (state == 4) {							//状態４（point９以上から）　4は初期セット状態（state=4）と移動状態（state=4.1)の2つに状態をわける		
-				zoyamaImg.y = Math.random() * 400;			//ｙ座標の位置をランダムに決定
+				kirimochiImg.y = Math.random() * 400;			//ｙ座標の位置をランダムに決定
 				state = 4.1;
 			}
 			if (state == 4.1) {							//状態４．１ 移動状態
-				zoyamaImg.x += 10;						//ただ移動します
+				kirimochiImg.x += 10;						//ただ移動します
 			}
 			if (state == 5) {							//状態５（point１２以上から）　 ｙ軸が毎フレーム毎に変化する
-				zoyamaImg.x += 20;						//移動します。
-				zoyamaImg.y = Math.random() * 400;			//ｙ座標の位置を枚フレーム毎にランダム決定
+				kirimochiImg.x += 20;						//移動します。
+				kirimochiImg.y = Math.random() * 400;			//ｙ座標の位置を枚フレーム毎にランダム決定
 			}
 
 			//現在のテキスト表示
 			scoreText.text = "現在：" + point; 				//point変数が変化するので、毎フレームごとにpointの値を読み込んだ文章を表示する
 
 			//ゲームオーバー判定
-			if (zoyamaImg.x >= 400) {						//画面端にぞう山画像が行ってしまったら
+			if (kirimochiImg.x >= 400) {						//画面端にぞう山画像が行ってしまったら
 				game.popScene();					//mainSceneシーンを外す
 				game.pushScene(endScene);				//endSceneシーンを読み込ませる
 
